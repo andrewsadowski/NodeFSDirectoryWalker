38cdcc51778540897e5b98fa156d3a93e08313d9
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-05-04 22:11:50 -0700

    refactored generateSub2TSV into multiple functions

--- subdirTest.js
+++ subdirTest.js
@@ -2,13 +2,10 @@ const fs = require('fs');
 const parser = require('subtitles-parser');
 
 const dirPath = 'srtTest/';
-const dataAdd = 'ADDING ENGLISH';
+const outputPath = 'output/';
 
 //Directory Reader
 const directoryReader = () => {
-  let fileArr = [];
-  let dirPathArr = [];
-
   fs.readdir(dirPath, (err, filesPath) => {
     console.log('Reading directory...');
     if (err) throw err;
@@ -16,47 +13,37 @@ const directoryReader = () => {
     filesPath = filesPath.map(filePath => {
       // fileArr.push(filePath);
       // dirPathArr.push(dirPath);
-      console.log(fileArr, dirPathArr);
       return dirPath + filePath;
-      console.log({ dirPath, filePath });
+      console.log(filesPath);
     });
-
-    console.log(filesPath);
-
     filesPath.map(filePath => {
-      // console.log(filePath);
       let subData = getSubData(filePath);
-      // console.log(subData);
-      generateSub2TSV(subData, filePath);
-      // generateSub2TSV(filePath);
+      console.log(subData);
 
-      // fs.appendFile(filePath, dataAdd, err => {
-      //   if (err) throw err;
-      //   console.log(`${filePath} has been appended`);
-      // });
+      generateSub2TSV(subData, filePath);
     });
   });
 };
 
 directoryReader();
 
+//Formats Srt file data into Object for Parsing
 const getSubData = filePath => {
   let srt = fs.readFileSync(filePath, 'utf8');
-  //Stores contents of subtitle file to array//
   let data = parser.fromSrt(srt);
-
   return data;
 };
 
 //subtitle => TSV
 const generateSub2TSV = (data, filePath) => {
-  //Parse subtitle
-
+  console.log(filePath);
+  filePath = filePath.replace(/srtTest\//, outputPath);
   data.forEach(sub => {
     let output = `${sub.id}\t${sub.startTime} --> ${sub.endTime}\t${sub.text}\n`;
-    // console.log(output);
-    // let newPath = filepath.replace(//, /g)
-    fs.appendFile(`./${filePath}.tsv`, output, err => {
+    let sortedOutput = output.sort((a, b) => {
+      return a[0] - b[0];
+    });
+    fs.appendFile(`./${filePath}.tsv`, sortedOutput, err => {
       if (err) return;
     });
   });
